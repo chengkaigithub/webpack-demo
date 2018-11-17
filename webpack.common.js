c29184e5d613285d19be6f53fa92f1c0f76e07f3
chengkai <cheng_kai@suixingpay.com>
2018-11-17 10:47:16 +0800

    加入图片压缩

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const imageMinPngQuant = require("imagemin-pngquant");
+const imageMinPngQuant = require("imagemin-pngquant");
 
 module.exports = {
   plugins: [
@@ -47,16 +47,16 @@ module.exports = {
             }
           },
           // img-loader for zip img
-          // {
-          //   loader: "img-loader",
-          //   options: {
-          //     plugins: [
-          //       imageMinPngQuant({
-          //         quality: "80" // the quality of zip
-          //       })
-          //     ]
-          //   }
-          // }
+          {
+            loader: "img-loader",
+            options: {
+              plugins: [
+                imageMinPngQuant({
+                  quality: "80" // the quality of zip
+                })
+              ]
+            }
+          }
         ]
       },
       {
