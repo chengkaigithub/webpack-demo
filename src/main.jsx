8dea355b76712de03d8e756d5b08a5708ec0ce51
chengkai <cheng_kai@suixingpay.com>
2018-11-19 14:06:23 +0800

    添加Loadable插件

--- src/main.jsx
+++ src/main.jsx
@@ -5,20 +5,25 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Loadable from "react-loadable";
 import './main.css';
 
 class Main extends React.Component {
 
   // componentDidMount() {
-    // import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
-    //   console.log(_.join(['lodash', 'loaded', 'finish'], '->'));
-    // }).catch(error => 'An error occurred while loading the component');
+  // import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
+  //   console.log(_.join(['lodash', 'loaded', 'finish'], '->'));
+  // }).catch(error => 'An error occurred while loading the component');
   // }
 
   clickImg = () => {
-    import(/* webpackChunkName: "print" */ './print').then(module => {
-      var print = module.default;
-      print();
+    // import(/* webpackChunkName: "print" */ './print').then(module => {
+    //   var print = module.default;
+    //   print();
+    // });
+    Loadable({
+      loader: () => import(/* webpackChunkName: "print" */ "./print"),
+      loading: <div style={{ width: '200px', height: '200px', backgroundColor: 'pink' }}>加载中...</div>
     });
   }
 
