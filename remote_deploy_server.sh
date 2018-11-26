3de9c7cf134e70583a9ef69ce6f756506fba134b
chengkai <cheng_kai@suixingpay.com>
2018-11-26 10:17:12 +0800

    本地部署脚本

--- remote_deploy_server.sh
+++ remote_deploy_server.sh
@@ -1,3 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/expect
 
-ssh root@47.91.157.69 "cd /usr/chengkai/test-project/webpack-demo; pwd; sh product.sh;"
+set timeout -1
+spawn ssh root@47.91.157.69
+expect "*password:" { send "Chengkai92\n" }
+expect "Welcome*" { send "pwd\r" }
+expect "*root" { send "cd /usr/chengkai/test-project/webpack-demo\r" }
+expect "*webpack-demo" { send "sh product.sh\r" }
+expect "*Done" { send "exit\r" }
+interact
+
+# 手动输入密码脚本
+# #!/usr/bin/env bash
+# ssh root@47.91.157.69 "cd /usr/chengkai/test-project/webpack-demo; pwd; sh product.sh;"
