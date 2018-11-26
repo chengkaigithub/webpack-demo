dc399c4f7e7eced8b1511e79ddc7702d6827152e
chengkai <cheng_kai@suixingpay.com>
2018-11-26 10:24:01 +0800

    远程部署脚本更新

--- remote_deploy_server.sh
+++ remote_deploy_server.sh
@@ -3,8 +3,7 @@
 set timeout -1
 spawn ssh root@47.91.157.69
 expect "*password:" { send "Chengkai92\n" }
-expect "Welcome*" { send "pwd\r" }
-expect "*root" { send "cd /usr/chengkai/test-project/webpack-demo\r" }
+expect "Welcome*" { send "cd /usr/chengkai/test-project/webpack-demo\r" }
 expect "*webpack-demo" { send "sh product.sh\r" }
 expect "*Done" { send "exit\r" }
 interact
