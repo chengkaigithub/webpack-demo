98dd0e3194b7fb041a6d7b3ef4253bae18763c6e
chengkai <cheng_kai@suixingpay.com>
2018-11-26 09:31:12 +0800

    commit remote script

---
+++ local_script.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ssh root@47.91.157.69 "cd /usr/chengkai/test-project/webpack-demo; pwd; sh product.sh;"
--- product.sh
+++ product.sh
@@ -4,6 +4,7 @@ git pull
 rm -rf node_modules
 # npm install
 # npm run build
+export yarn=/usr/local/yarn/yarn-v1.7.0/bin/yarn
 yarn
 yarn build
 nginx -s reload
