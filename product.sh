1319d5a389edf676dd68131fe67ccea6322d1a40
chengkai <cheng_kai@suixingpay.com>
2018-11-26 09:37:28 +0800

    添加远程更新脚本

--- product.sh
+++ product.sh
@@ -4,7 +4,6 @@ git pull
 rm -rf node_modules
 # npm install
 # npm run build
-export yarn=/usr/local/yarn/yarn-v1.7.0/bin/yarn
-yarn
-yarn build
+/usr/local/yarn/yarn-v1.7.0/bin/yarn
+/usr/local/yarn/yarn-v1.7.0/bin/yarn build
 nginx -s reload
