eb93da7d55963312ec721b10f188e91ddb0a440a
chengkai <cheng_kai@suixingpay.com>
2018-12-22 14:47:30 +0800

    修改打包命令

--- product.sh
+++ product.sh
@@ -6,4 +6,9 @@ rm -rf node_modules
 # npm run build
 /usr/local/yarn/yarn-v1.7.0/bin/yarn
 /usr/local/yarn/yarn-v1.7.0/bin/yarn build
+
+cd /usr/chengkai/test-project/webpack-demo/build
+rm -rf *
+mv -f /usr/chengkai/test-project/webpack-demo/build/* /usr/chengkai/test-project/webpack-demo/product/
+
 nginx -s reload
