ed3fdf1be1ebd35a3922a97d72ac5d5dd9a00a15
chengkai <cheng_kai@suixingpay.com>
2019-02-10 16:30:48 +0800

    init react-scaffold

--- src/polyfills.js
+++
@@ -1,6 +0,0 @@
-/**
- * Create by chengkai on 2018/11/20.
- * Describe: 兼容库
- */
-import 'babel-polyfill';
-import 'whatwg-fetch';
--- src/redux-base/store/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Create by chengkai on 2018/11/27.
- * Describe:
- */
-
-import { createStore, applyMiddleware, compose } from 'redux';
-import reducers from '../reducers';
-
-export default createStore(reducers);
